aa1c425a2b1c931b845ea370643c7da5435fef7b
lihan <lihan@apache.org>
2024-03-14 17:00:51 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# vfox-plugin-template
---
+++ metadata.lua
@@ -0,0 +1,23 @@
+--- !!! DO NOT EDIT OR RENAME !!!
+PLUGIN = {}
+
+--- !!! MUST BE SET !!!
+--- Plugin name
+PLUGIN.name = "java"
+--- Plugin author
+PLUGIN.author = "Lihan"
+--- Plugin version
+PLUGIN.version = "0.0.1"
+-- Update URL
+PLUGIN.updateUrl = "{URL}/sdk.lua"
+
+--- !!! OPTIONAL !!!
+--- Plugin description
+PLUGIN.description = "xxx"
+-- minimum compatible vfox version
+PLUGIN.minRuntimeVersion = "0.2.2"
+-- legacy filenames
+PLUGIN.legacyFilenames = {
+    ".nvmrc",
+    ".node-version",
+}
