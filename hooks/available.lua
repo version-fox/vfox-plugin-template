fd312b5631f3dc5c0c5a306a48c98e66c6e649c0
lihan <lihan@apache.org>
2024-03-19 15:15:44 +0800

    mod: template

--- hooks/available.lua
+++ hooks/available.lua
@@ -1,7 +1,10 @@
+local util = require("util")
+
 --- Return all available versions provided by this plugin
 --- @param ctx table Empty table used as context, for future extension
 --- @return table Descriptions of available versions and accompanying tool descriptions
 function PLUGIN:Available(ctx)
+    util:DoSomeThing()
     local runtimeVersion = ctx.runtimeVersion
     return {
         {
