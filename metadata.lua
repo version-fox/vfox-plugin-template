3f89d7e512a6a321fe2f76069e61a491bd1958b1
lihan <lihan@apache.org>
2024-03-19 17:46:22 +0800

    mod

--- metadata.lua
+++ metadata.lua
@@ -5,15 +5,17 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "java"
 --- Plugin author
-PLUGIN.author = "Lihan"
+PLUGIN.author = "your name"
 --- Plugin version
 PLUGIN.version = "0.0.1"
 --- Plugin repository
-PLUGIN.repository = "https://github.com/version-fox/vfox-plugin-template"
-
---- !!! OPTIONAL !!!
+PLUGIN.homepage = "https://github.com/version-fox/vfox-plugin-template"
+--- Plugin license
+PLUGIN.license = "MIT"
 --- Plugin description
 PLUGIN.description = "xxx"
+
+--- !!! OPTIONAL !!!
 -- minimum compatible vfox version
 PLUGIN.minRuntimeVersion = "0.2.2"
 -- legacy filenames
