d17be59e00eb42917482b0cc644208ea3da70dfc
Han Li <lihan@apache.org>
2024-03-20 16:52:25 +0800

    Generate Manifest and Publish (#1)

--- metadata.lua
+++ metadata.lua
@@ -5,19 +5,26 @@ PLUGIN = {}
 --- Plugin name
 PLUGIN.name = "java"
 --- Plugin author
-PLUGIN.author = "Lihan"
+PLUGIN.author = "your name"
 --- Plugin version
 PLUGIN.version = "0.0.1"
--- Update URL
-PLUGIN.updateUrl = "{URL}/sdk.lua"
-
---- !!! OPTIONAL !!!
+--- Plugin repository
+PLUGIN.homepage = "https://github.com/version-fox/vfox-plugin-template"
+--- Plugin license
+PLUGIN.license = "MIT"
 --- Plugin description
 PLUGIN.description = "xxx"
+
+--- !!! OPTIONAL !!!
 -- minimum compatible vfox version
 PLUGIN.minRuntimeVersion = "0.2.2"
 -- legacy filenames
 PLUGIN.legacyFilenames = {
     ".nvmrc",
     ".node-version",
+}
+
+PLUGIN.notes ={
+    "some thing",
+    "some thing",
 }
